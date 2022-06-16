f0e4edf200883a0405f4f41f38f6fd1cdb6f6ed4
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-16 08:22:43 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,62 @@
+/*Reverse a String 
+Basic Accuracy: 56.22% Submissions: 100k+ Points: 1
+You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+Your Task:
+
+You only need to complete the function reverseWord() that takes s as parameter and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000*/
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        String temp="";
+  for(int x=str.length()-1;x>=0;x--){
+      temp+=str.charAt(x);
+  }
+  return temp;
+    }
+}
