8fd3d7b35c3fe9bc154674be570ad1d53032ee7f
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-19 07:42:37 +0530

    Create RemoveConsecutiveCharacters.java

---
+++ RemoveConsecutiveCharacters.java
@@ -0,0 +1,78 @@
+Remove Consecutive Characters 
+/*Basic Accuracy: 55.83% Submissions: 19923 Points: 1
+Given a string S delete the characters which are appearing more than once consecutively.
+
+Example 1:
+
+Input:
+S = aabb
+Output:  ab 
+Explanation: 'a' at 2nd position is
+appearing 2nd time consecutively.
+Similiar explanation for b at
+4th position.
+
+Example 2:
+
+Input:
+S = aabaa
+Output:  aba
+Explanation: 'a' at 2nd position is
+appearing 2nd time consecutively.
+'a' at fifth position is appearing
+2nd time consecutively.
+ 
+
+Your Task:
+You dont need to read input or print anything. Complete the function removeConsecutiveCharacter() which accepts a string as input parameter and returns modified string.
+ 
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(|S|).
+ 
+
+Constraints:
+1<=|S|<=105
+All characters are lowercase alphabets.
+*/
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            String S = read.readLine().trim();
+            
+            Solution ob = new Solution();
+            String ans = ob.removeConsecutiveCharacter(S);
+            
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public String removeConsecutiveCharacter(String S){
+         String ans=""+S.charAt(0);
+        int x=1;
+        int n=S.length();
+        while(x<n){
+            while(x<n && S.charAt(x)==S.charAt(x-1))
+            x++;
+            if(x<n)
+            ans=ans+S.charAt(x++);
+        }
+        return ans;
+    }
+}
