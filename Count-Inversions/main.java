c51ab80dd1a67dcab7bf5fb689e0bca1d69e80f3
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-24 12:57:03 +0530

    Update and rename Count-Inversions.java to Count-Inversions/main.java

---
+++ Count-Inversions/main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Sorting
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+            long arr[] = new long[(int)n];
+            
+            for(long i = 0; i < n; i++)
+             arr[(int)i] = sc.nextLong();
+             
+            System.out.println(new Solution().inversionCount(arr, n));
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    //Function to count inversions in the array.
+    static long inversionCount(long arr[], long N)
+    {
+        long count=0L;
+        for(int x=0;x<N;x++){
+            for(int y=x+1;y<N;y++){
+                if(arr[x]>arr[y])
+                count++;
+            }
+        }
+        return count;
+    }
+}
