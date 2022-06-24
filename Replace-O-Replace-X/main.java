958b778b2d7748d3c4d29e733b71dfd1fd7488ca
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-24 13:27:29 +0530

    Update and rename Replace-O-Replace-X.java to Replace-O-Replace-X/main.java

--- Replace-O-Replace-X/main.java
+++ Replace-O-Replace-X/main.java
@@ -1,37 +1,3 @@
-/*
-Replace O's with X's 
-Medium Accuracy: 45.14% Submissions: 4756 Points: 4
-Given a matrix mat of size N x M where every element is either ‘O’ or ‘X’.
-Replace all ‘O’ with ‘X’ that are surrounded by ‘X’.
-A ‘O’ (or a set of ‘O’) is considered to be by surrounded by ‘X’ if there are ‘X’ at locations just below, just above, just left and just right of it.
-
-Example 1:
-
-Input: n = 5, m = 4
-mat = {{'X', 'X', 'X', 'X'}, 
-       {'X', 'O', 'X', 'X'}, 
-       {'X', 'O', 'O', 'X'}, 
-       {'X', 'O', 'X', 'X'}, 
-       {'X', 'X', 'O', 'O'}}
-Output: ans = {{'X', 'X', 'X', 'X'}, 
-               {'X', 'X', 'X', 'X'}, 
-               {'X', 'X', 'X', 'X'}, 
-               {'X', 'X', 'X', 'X'}, 
-               {'X', 'X', 'O', 'O'}}
-Explanation: Following the rule the above 
-matrix is the resultant matrix. 
-Your Task:
-You do not need to read input or print anything. Your task is to complete the function fill() which takes n, m and mat as input parameters ad returns a 2D array representing the resultant matrix.
-
-Expected Time Complexity: O(n*m)
-Expected Auxiliary Space: O(n*m)
-
-Constraints:
-1 ≤ n, m ≤ 500
-*/
-// { Driver Code Starts
-//Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
