f060256f4ede58b81a4e9fd369ec60daf508385c
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-24 13:18:00 +0530

    Update and rename Largest-Number-Formed-From-Array.java to Largest-Number-Formed-From-Array/main.java

---
+++ Largest-Number-Formed-From-Array/main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] arr = br.readLine().trim().split(" ");
+
+            String ans = new Solution().printLargest(arr);
+            System.out.println(ans);
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String printLargest(String[] arr) {
+        Arrays.sort(arr);
+        String s="";
+        for(int x=arr.length-1;x>=0;x--)
+        s=s+arr[x];
+        
+        return s;
+    }
+}
