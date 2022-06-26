4a880c62034b444327d52dd31c63ec1167dd77ab
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-26 06:59:56 +0530

    Update main.java

--- Find All Four Sum Numbers/main.java
+++ Find All Four Sum Numbers/main.java
@@ -1,91 +1,60 @@
-// { Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-            String[] nk = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(nk[0]);
-            int k = Integer.parseInt(nk[1]);
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[n];
-            for (int i = 0; i < n; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-
-            Solution sln = new Solution();
-            ArrayList<ArrayList<Integer>> ans = sln.fourSum(a, k);
-            for (ArrayList<Integer> v : ans) {
-                for (int u : v) {
-                    System.out.print(u + " ");
-                }
-                System.out.print("$");
-            }
-            if (ans.isEmpty()) {
-                System.out.print(-1);
-            }
-            System.out.println();
-        }
-    }
-}// } Driver Code Ends
-
-
-// User function Template for Java
-
-// arr[] : int input array of integers
-// k : the quadruple sum required
-
-class Solution {
-    public ArrayList<ArrayList<Integer>> fourSum(int[] arr, int k) {
-        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
-        if(arr==null || arr.length==0)
-        return ans;
-        
-        Arrays.sort(arr);
-        int len=arr.length;
-        
-        for(int x=0;x<len;x++){
-         int sum=k-arr[x];
-         for(int y=x+1;y<len;y++){
-             int sum2=sum-arr[y];
-             int left=y+1;
-             int right=len-1;
-             while(left<right){
-                 int sum3=arr[right]+arr[left];
-                 if(sum3<sum2)
-                 left++;
-                 else if(sum3>sum2)
-                 right--;
-                 else{
-                     ArrayList<Integer> temp=new ArrayList<>();
-                     temp.add(arr[x]);
-                     temp.add(arr[y]);
-                     temp.add(arr[left]);
-                     temp.add(arr[right]);
-                     ans.add(temp);
-                     while(left<right && temp.get(2)==arr[left])
-                     ++left;
-                     while(left<right && temp.get(3)==arr[right])
-                     --right;
-                     
-                 }
-                 
-             }
-             while(y+1<len && arr[y+1]==arr[y])
-             ++y;
-            
-         }
-         while(x+1<len && arr[x+1]==arr[x])
-         ++x;
-        }
-        return ans;
-    }
-}
+public ArrayList<ArrayList<Integer>> fourSum(int[] arr, int k) {
+       // code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+       
+       if (arr == null || arr.length == 0)
+           return res;
+       
+       int n = arr.length;
+   
+       Arrays.sort(arr); 
+   
+       for (int i = 0; i < n; i++) {
+       
+           int target_3 = k - arr[i];
+       
+           for (int j = i + 1; j < n; j++) {
+           
+               int target_2 = target_3 - arr[j];
+           
+               int front = j + 1;
+               int back = n - 1;
+           
+               while(front < back) {
+               
+                   int two_sum = arr[front] + arr[back];
+               
+                   if (two_sum < target_2) front++;
+               
+                   else if (two_sum > target_2) back--;
+               
+                   else {
+                   
+                       ArrayList<Integer> quad = new ArrayList<>(); 
+                       quad.add(arr[i]);
+                       quad.add(arr[j]);
+                       quad.add(arr[front]);
+                       quad.add(arr[back]);
+                       res.add(quad);
+                   
+                       // Processing the duplicates of number 3
+                       while (front < back && arr[front] == quad.get(2)) ++front;
+                   
+                       // Processing the duplicates of number 4
+                       while (front < back && arr[back] == quad.get(3)) --back;
+               
+                   }
+               }
+               
+               // Processing the duplicates of number 2
+               while(j + 1 < n && arr[j + 1] == arr[j]) ++j;
+           }
+       
+           // Processing the duplicates of number 1
+           while (i + 1 < n && arr[i + 1] == arr[i]) ++i;
+       
+       }
+       
+   
+       return res;
+   }
